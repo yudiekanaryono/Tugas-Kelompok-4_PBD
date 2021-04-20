2a44bbacae8d3753aea56c9e1480feff06f6ee3d
Rista01 <66737962+Rista01@users.noreply.github.com>
2021-04-21 06:59:56 +0700

    Update and rename 19753065_curd_4.php to admin.php

--- admin.php
+++ admin.php
@@ -53,6 +53,13 @@ mysqli_close($hub);
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
     <title>Welcome !</title>
+	<script type ="text/javascript" language="JavaScript">
+	function konfirmasi()
+	{
+		tanya = confirm("Anda Yakin Akan Menghapus");
+		if (tanya==true) return true;
+		else return false;
+	}</script>
   </head>
   <body background="pipes.png">
     
@@ -65,12 +72,12 @@ function read_data() {
 
 	  <div style="max-width: 600px; margin: 9em auto">
 
-	<h2 class="text-center">Read Data Program Studi
+	<h2 class="text-center">Data Program Studi
 	 <a class="nav-link" href="home.php">HOME<span class="sr-only">(current)</span></a></h2>
 	<hr>
 <table class="table table-bordered table-striped">
 		<tr>
-			<td colspan="5"><a href="19753065_curd_4.php?a=input">INPUT</a></td>
+			<td colspan="5"><a href="admin.php?a=input">INPUT</a></td>
 		</tr>
 		<tr>
 			<td>ID</td>
@@ -87,8 +94,8 @@ function read_data() {
 			<td><?php echo $row['akreditas']; ?></td>
 			<td>
 
-				 <button type="button" class="btn btn-outline-success"> <a href="19753065_curd_4.php?a=edit&id=<?php echo $row ['idprodi']; ?>">EDIT</a></button>
-        <button type="button" class="btn btn-outline-warning"><a href="19753065_curd_4.php?a=hapus&id=<?php echo $row ['idprodi']; ?>">HAPUS</a></button>
+				 <button type="button" class="btn btn-outline-success"> <a href="admin.php?a=edit&id=<?php echo $row ['idprodi']; ?>">EDIT</a></button>
+        <button type="button" class="btn btn-outline-warning"><a href="admin.php?a=hapus&id=<?php echo $row ['idprodi']; ?>">HAPUS</a></button>
 				
 				
 			</td>
@@ -106,25 +113,37 @@ function input_data(){
 			"akreditas" => "-"
 			); ?>
 
-	<h2>Input Data Program Studi</h2>
-	<form action="19753065_curd_4.php?a=input" method="post"> 
+	<h2><center>Input Data Program Studi</center></h2>
+	<table class="table table-bordered table-striped">
+	<form action="admin.php?a=input" method="post"> 
 		<input type="hidden" name="sql" value="create"> 
-		Kode Prodi
-		<input type="text" colspan="5" name="kdprodi" maxlength="6" size="6" value="<?php echo trim($row["kdprodi"]) ?>" />
-		<br><br>
-		Nama Prodi
-		<input type="text" colspan="5" name="nmprodi" maxlength="70" size="70" value="<?php echo trim($row["nmprodi"]) ?>" />
-		<br><br>
+		<tr>
+		<td>Kode Prodi</td>
+		<td><input type="text" name="kdprodi" maxlength="6" size="6" value="<?php echo trim($row["kdprodi"]) ?>" /></td>
+		</tr>
+		<tr>
+		<td>
+		Nama Prodi</td>
+		<td><input type="text" name="nmprodi" maxlength="70" size="70" value="<?php echo trim($row["nmprodi"]) ?>" />
+		</td>
+		</tr>
+		<tr>
+		<td>
 		akreditas Prodi
+		</td>
+		<td>
 		<input type="radio" name="akreditas" value="-" <?php if($row["akreditas"]=='-' || $row["akreditas"]=='') { echo "checked=\"checked\""; } else {echo "";} ?>> -
 		<input type="radio" name="akreditas" value="A" <?php if($row["akreditas"]=='A') { echo "checked=\"checked\""; } else {echo "";} ?>> A
 		<input type="radio" name="akreditas" value="B" <?php if($row["akreditas"]=='B') { echo "checked=\"checked\""; } else {echo "";} ?>> B
 		<input type="radio" name="akreditas" value="C" <?php if($row["akreditas"]=='C') { echo "checked=\"checked\""; } else {echo "";} ?>> C
-		<br><br>
-		<input type="submit" class="btn btn-outline-success" name="action" value="Simpan">
-		<br>
-		<a href="19753065_curd_4.php?a=list" class="btn btn-outline-warning">Batal</a>
+		</td>
+	</tr>
+	<tr>
+	<td colspan="2" align ="right"> <button type="submit" name = "action" class="btn btn-outline-success" value "Simpan">Simpan</button>
+        <button type="button" class="btn btn-outline-warning"><a href="admin.php?a=list"</a>Batal</button></td>
+		</tr>
 	</form>
+	</table>
 <?php } ?>
 
 <?php
@@ -135,25 +154,36 @@ function edit_data($id ){
 	$row = mysqli_fetch_array($result); ?>
 
 <h2>Edit Data Program Studi</h2>
-	<form action="19753065_curd_4.php?a=list" method="post">
+<table class="table table-bordered table-striped">
+	<form action="admin.php?a=list" method="post">
 		<input type="hidden" name="sql" value="update">
 		<input type="hidden" name="idprodi" value="<?php echo trim ($id) ?>">
-		Kode Prodi
-		<input type="text" colspan="5" name="kdprodi" maxlength="70" size="70" value="<?php echo trim($row["kdprodi"]) ?>" />
-		<br><br>
-		Nama Prodi 
-		<input type="text" name="nmprodi" maxlength="70" size="70" value="<?php echo trim($row["nmprodi"]) ?>" />
-		<br><br>
-		Akreditas Prodi
-		<input type="radio" name="akreditas" value="-" <?php if ($row["akreditas"]=="-" || $row["akreditas"]=='') { echo "checked=\"checked\""; } else {echo ""; } ?>> -
-		<input type="radio" name="akreditas" value="A" <?php if ($row["akreditas"]=='A') {echo "checked=\"checked\""; } else {echo ""; } ?> > A
-		<input type="radio" name="akreditas" value="B" <?php if ($row["akreditas"]=='B') {echo "checked=\"checked\""; } else {echo ""; } ?>> B
-		<input type="radio" name="akreditas" value="C" <?php if ($row["akreditas"]=='C') {echo "checked=\"checked\""; } else {echo ""; } ?>> C
-		<br><br>
-		<input type="submit"class="btn btn-outline-success" name="action" value="Simpan">
-		<br>
-		<a href="19753065_curd_4.php?a=list" class="btn btn-outline-warning">Batal</a>
+		<tr>
+		<td>Kode Prodi</td>
+		<td><input type="text" name="kdprodi" maxlength="6" size="6" value="<?php echo trim($row["kdprodi"]) ?>" /></td>
+		</tr>
+		<tr>
+		<td>
+		Nama Prodi</td>
+		<td><input type="text" name="nmprodi" maxlength="70" size="70" value="<?php echo trim($row["nmprodi"]) ?>" />
+		</td>
+		</tr>
+		<tr>
+		<td>
+		akreditas Prodi
+		</td>
+		<td>
+		<input type="radio" name="akreditas" value="-" <?php if($row["akreditas"]=='-' || $row["akreditas"]=='') { echo "checked=\"checked\""; } else {echo "";} ?>> -
+		<input type="radio" name="akreditas" value="A" <?php if($row["akreditas"]=='A') { echo "checked=\"checked\""; } else {echo "";} ?>> A
+		<input type="radio" name="akreditas" value="B" <?php if($row["akreditas"]=='B') { echo "checked=\"checked\""; } else {echo "";} ?>> B
+		<input type="radio" name="akreditas" value="C" <?php if($row["akreditas"]=='C') { echo "checked=\"checked\""; } else {echo "";} ?>> C
+		</td>
+	</tr>
+		<tr>
+		<td colspan="2" align ="right"> <button type="submit" name="action" class="btn btn-outline-success" value "Simpan">Simpan</button><button type="button" class="btn btn-outline-warning"><a href="admin.php?a=list"</a>Batal</button>
+		</tr>
 	</form>
+	</table>
 <?php }  ?>
 <?php
 function hapus_data($id) {
@@ -162,12 +192,12 @@ function hapus_data($id) {
 	$result = mysqli_query($hub, $query);
 	$row = mysqli_fetch_array($result);?>
 <h2> Hapus Data Program Studi</h2>
-<form action="19753065_curd_4.php?a=list" method="post">
+<form action="admin.php?a=list" method="post">
 	<input type="hidden" name="sql" value="delete">
 	<input type="hidden" name="idprodi" value="<?php echo trim($id)?>">
-	<table>
+	<table class="table table-bordered table-striped">
 		<tr>
-			<td width=100>kode</td>
+			<td width=100>Kode</td>
 			<td><?php echo trim($row["kdprodi"])?></td>
 		</tr>
 		<tr>
@@ -180,9 +210,9 @@ function hapus_data($id) {
 		</tr>
 	</table>
 	<br>
-	<input type="submit" name="action" value="Hapus">
+	<input type="submit" onclick="return konfirmasi()" name="action" value="Hapus">
 	<br>
-	<a href="19753065_curd_4.php?a=list">Batal</a>
+	<a href="admin.php?a=list">Batal</a>
 	</form>
 	<?php } ?>
 <?php 
@@ -220,5 +250,3 @@ function delete_prodi() {
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
   </body>
 </html>
-
-
