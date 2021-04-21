2029cc341316be19f7e77f9a059175e1e5b586fc
mutiaraermandani <82753302+mutiaraermandani@users.noreply.github.com>
2021-04-21 12:03:26 +0700

    Add files via upload

--- homeclient.php
+++ homeclient.php
@@ -4,14 +4,15 @@ session_start();
 <!doctype html>
 <html lang="en">
   <head>
-    <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Jekyll v3.8.5">
     <title>PROJECT KELOMPOK 4</title>
+ 
     <link rel="canonical" href="https://getbootstrap.com/docs/4.3/examples/starter-template/">
+ 
     <!-- Bootstrap core CSS -->
 <link href="assets/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" 
 crossorigin="anonymous">
@@ -42,7 +43,7 @@ crossorigin="anonymous">
   aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
- <a class="navbar-brand" href="#"><?php echo $_SESSION['role']; ?></a>
+  <a class="navbar-brand" href="#"><?php echo $_SESSION['role']; ?></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" 
   aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
@@ -53,7 +54,7 @@ crossorigin="anonymous">
         <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item active">
-        <a class="nav-link" href="admin.php">Data Prodi<span class="sr-only">(current)</span></a>
+        <a class="nav-link" href=" client.php">Data Prodi<span class="sr-only">(current)</span></a>
       </li>
     </ul>
     
